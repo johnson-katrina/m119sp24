b7181bc7a71785b0c0b254296198efda4d341bb7
johnson-katrina <johnsonkat@byui.edu>
2024-05-06 11:53:51 -0600

    after class May 6

---
+++ Day9_May6.R
@@ -0,0 +1,17 @@
+?log()
+
+log(17,2)
+log2(17)
+log(17)
+log(17,10)
+log10(17)
+
+x <- seq(-10,10,0.1)
+f <- function(x){2^x}
+plot(x,f(x),type='l')
+lines(x,exp(x),col=2)
+
+exp(1)
+
+log(2)
+exp(log(2))
